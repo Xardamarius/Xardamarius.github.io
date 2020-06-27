caa45fd034d68fc19f47755d10e9039eaec70964
Xardamarius <59386350+Xardamarius@users.noreply.github.com>
2020-06-27 16:52:14 +0800

    Update script.js

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,3 +1,24 @@
+class Options {
+	constructor(height, width, bg, fontSize, textAlign) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+	}
+
+	createDiv() {
+		let elem = document.createElement('div');
+		document.body.appendChild(elem);
+		let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+		elem.style.cssText = param;
+	}
+}
+
+const item = new Options(300, 350, "red", 14, "center");
+
+item.createDiv();
+
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
@@ -102,6 +123,8 @@ window.addEventListener('DOMContentLoaded', function() {
         more.classList.remove('more-splash');
         document.body.style.overflow = '';
     });
+
+    
 
 });
 
